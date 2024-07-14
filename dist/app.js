6a2b8a0b10651af91fa703a27b21de272827dbb8
FCICoder <mahmoud.aadel1998@gmail.com>
2024-07-14 08:42:35 +0300

    'last'

--- dist/app.js
+++ dist/app.js
@@ -8,26 +8,23 @@ config();
 const app = express();
 // !Middleware 
 // List of allowed origins
-const allowedOrigins = ['http://localhost:5173', 'https://ai-chat-bot-frontend-sigma.vercel.app'];
-// CORS options delegate function
-const corsOptionsDelegate = function (req, callback) {
-    let corsOptions;
-    if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true, credentials: true }; // Enable CORS for this request
-    }
-    else {
-        corsOptions = { origin: false }; // Disable CORS for this request
-    }
-    callback(null, corsOptions); // Callback expects two parameters: error and options
-};
+// const allowedOrigins = ['http://localhost:5173', 'https://ai-chat-bot-frontend-sigma.vercel.app'];
+// // CORS options delegate function
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions;
+//   if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true, credentials: true }; // Enable CORS for this request
+//   } else {
+//     corsOptions = { origin: false }; // Disable CORS for this request
+//   }
+//   callback(null, corsOptions); // Callback expects two parameters: error and options
+// };
 // Use the CORS middleware with the options delegate
-app.use(cors(corsOptionsDelegate));
-// app.use(cors(
-//     {
-//         origin: 'http://localhost:5173',
-//         credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-//       }
-// ))
+// app.use(cors(corsOptionsDelegate));
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://ai-chat-bot-frontend-phi.vercel.app'],
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // ! remove it in production
